447edfd19b379c76a731698aeb385b9dac34ff40
Пользователь <murodjontohirov2701@gmail.com>
2022-07-01 19:04:50 +0500

    logical operators02

--- bin/logical_operators02.dart
+++ bin/logical_operators02.dart
@@ -8,4 +8,18 @@ Given two integers a, b,  check the following statement "Each of the numbers 'a'
     Returns:
         bool: answer
 */
-void main() {}
+import 'package:test/expect.dart';
+
+bool func(int a, int b) {
+  if (a > 0 && b > 0) {
+    return true;
+  }
+  if (a < 0 && b > 0) {
+    return false;
+  }
+  return false;
+}
+
+void main() {
+  print(func(4, 9));
+}
